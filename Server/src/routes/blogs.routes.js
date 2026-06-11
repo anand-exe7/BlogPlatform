646f158de4c020d33bf9f6c731a2d25f000031e7
anand-exe7 <anandsiva0317@gmail.com>
2026-06-11 15:21:42 +0530

    final push now all the image will be stored in cloudflare and the image url will be stored in the neondb

--- Server/src/routes/blogs.routes.js
+++ Server/src/routes/blogs.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import * as blogsController from "../controllers/blogs.controller.js";
 import { authenticate, optionalAuth } from "../middleware/auth.js";
 import { ensureMember } from "../middleware/role.js";
-import { uploadImage } from '../middleware/upload.js';
 
 const router = express.Router();
 
@@ -12,12 +11,6 @@ router.get("/public/:id", optionalAuth, blogsController.getPublicBlogById);
 
 // Member endpoints (require auth + member role)
 router.use(authenticate);
-router.post('/upload-image', uploadImage.single('image'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ success: false, error: { message: 'No file uploaded' } });
-  }
-  res.json({ success: true, data: { url: req.file.path } });
-});
 router.post("/", ensureMember, blogsController.createDraft);
 router.patch("/:id", ensureMember, blogsController.editDraft);
 router.delete("/:id", ensureMember, blogsController.deleteDraft);
